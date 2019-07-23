02f0c2a00b6d25a5fd71fcceda1cc0f1e54a121b
buresj <bures@fakultnisociologie.cz>
2019-07-23 13:57:08 +0200

    Updates

---
+++ precache-manifest.83dff9210e45f96c9cd72085b64afe19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aff95e5b6ccb4c173f5760b3c07c99c",
+    "url": "/sociologikon/index.html"
+  },
+  {
+    "revision": "933d8da753178ac23399",
+    "url": "/sociologikon/static/css/main.6113e04d.chunk.css"
+  },
+  {
+    "revision": "88701686358d1f9bf8dc",
+    "url": "/sociologikon/static/js/2.9ae4ff4b.chunk.js"
+  },
+  {
+    "revision": "933d8da753178ac23399",
+    "url": "/sociologikon/static/js/main.639ba1f3.chunk.js"
+  },
+  {
+    "revision": "d593ed5f8e2dae0a358a",
+    "url": "/sociologikon/static/js/runtime~main.f9e693b4.js"
+  }
+]);
