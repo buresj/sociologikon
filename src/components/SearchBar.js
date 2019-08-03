7b1e99a7f6b469df21c8bb2b64dcdba180da0f4d
buresj <bures@fakultnisociologie.cz>
2019-08-03 23:59:30 +0200

    debounce for search added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import SearchIcon from '@material-ui/icons/Search';
 import styles from '../SearchBar.module.scss';
 
 const SearchBar = (props) => {
+
+  const debounce = (func, delay) => {
+    let inDebounce
+    return function () {
+      const context = this
+      const args = arguments
+      clearTimeout(inDebounce)
+      inDebounce = setTimeout(() => func.apply(context, args), delay)
+    }
+  }
+
+  const delayedSearch = debounce(input => props.search(input), 400);
+
+  const handleChange = e => delayedSearch(e.target.value);
+
   return (
     <div className={styles.search}>
       <input
-        onChange={(event) => { props.search(event.target.value) }}
+        onChange={handleChange}
         placeholder={`Hledej`}
       />
     </div >
   );
 }
+
 export default SearchBar;
